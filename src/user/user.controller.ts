3bb65b2dee02a381b7db92f0e0ca84d7ccca5d5b
Egor Maximov <maximov.devs@gmail.com>
2022-02-16 19:32:45 +0300

    add user module

---
+++ src/user/user.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Post } from '@nestjs/common';
+import { UserService } from '@app/user/user.service';
+
+@Controller()
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+  @Post('users')
+  async createUser(): Promise<any> {
+    return await this.userService.createUser();
+  }
+}
